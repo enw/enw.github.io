4d918112fced38d4359f01b20534c257d1605e77
Elliot Winard <enw@caveteen.com>
2014-08-20 00:12:52 -0400

    initial commit of resume

---
+++ resume/gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
+gulp.task('serve', function () {
+    browserSync({
+        server: {
+            baseDir: '.'
+        }
+    });
+    gulp.watch(['*.html', '*.css', '*.js'],reload);
+});
